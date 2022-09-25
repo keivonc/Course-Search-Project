d004bb991108d5e3de644a14da6d1d483b107cf0
emilivanov21 <112448206+emilivanov21@users.noreply.github.com>
2022-09-25 13:20:13 -0400

    setting for heroku

--- louslist/louslist/settings.py
+++ louslist/louslist/settings.py
@@ -134,3 +134,11 @@ try:
         django_heroku.settings(locals())
 except ImportError:
     found = False
+
+ # Static files (CSS, JavaScript, Images)
+ # https://docs.djangoproject.com/en/3.0/howto/static-files/
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+os.path.join(BASE_DIR,'static')
+]
