3e8e4a5eba3388681cb6ae9d33d35d0ed91edbc8
mwc2dh <mwc2dh@virginia.edu>
2022-10-07 16:00:10 -0400

    updated DATABASES

--- louslist/settings.py
+++ louslist/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,23 +80,24 @@ WSGI_APPLICATION = 'louslist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': "d86g51mv1m453l",
-        'USER': "sqgrwrnmchzfmv",
-        'PASSWORD': "290003c4b66d93740d093412998dd270ce99ca51290a222f4447762f0d2c66e0",
-        'HOST': "ec2-35-170-146-54.compute-1.amazonaws.com",
-        'PORT':  "5432"
+if 'test' in sys.argv:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else: 
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': "d86g51mv1m453l",
+            'USER': "sqgrwrnmchzfmv",
+            'PASSWORD': "290003c4b66d93740d093412998dd270ce99ca51290a222f4447762f0d2c66e0",
+            'HOST': "ec2-35-170-146-54.compute-1.amazonaws.com",
+            'PORT':  "5432"
+        }
+    }
 
 
 # Password validation
