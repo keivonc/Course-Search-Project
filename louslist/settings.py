677e0c7e4b74be392cfc5619c1152884f72cb348
emilivanov21 <112448206+emilivanov21@users.noreply.github.com>
2022-10-07 13:15:35 -0400

    postgres

--- louslist/settings.py
+++ louslist/settings.py
@@ -79,10 +79,21 @@ WSGI_APPLICATION = 'louslist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "d86g51mv1m453l",
+        'USER': "sqgrwrnmchzfmv",
+        'PASSWORD': "290003c4b66d93740d093412998dd270ce99ca51290a222f4447762f0d2c66e0",
+        'HOST': "ec2-35-170-146-54.compute-1.amazonaws.com",
+        'PORT':  "5432"
     }
 }
 
