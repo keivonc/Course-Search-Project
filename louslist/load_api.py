57a8c401ccc2a7a55fbc7117e409a7f2fb8c103f
emilivanov21 <112448206+emilivanov21@users.noreply.github.com>
2022-10-13 10:46:04 -0400

    json

--- louslist/load_api.py
+++ louslist/load_api.py
@@ -1,4 +1,7 @@
 import json
+import urllib.request
+
+webUrl = urllib.request.urlopen('https://luthers-list.herokuapp.com/api/dept/APMA/')
 
 
 f = open('JSON/department.json')
@@ -8,10 +11,11 @@ f = open('JSON/department.json')
 data = json.load(f)
   
 # Iterating through the json
-# list
+#list
 for i in data:
     for value in i.items():
+        
         with open(value[1]+".json",'w') as f:
-            f.write("http://luthers-list.herokuapp.com/api/dept/"+value[1]+"/?format=json")
+            f.write(webUrl+value[1]+"/?format=json")
        
 
