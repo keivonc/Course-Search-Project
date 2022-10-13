548f5146b80ec182c5fd535f11b396875bfb2b0d
emilivanov21 <112448206+emilivanov21@users.noreply.github.com>
2022-10-13 10:34:27 -0400

    json

--- louslist/load_api.py
+++ louslist/load_api.py
@@ -1,4 +1,17 @@
 import json
 
-data = open('/JSON/JSON.json').read() #opens the json file and saves the raw contents
-jsonData = json.loads(data) #converts to a json structure
+
+f = open('JSON/department.json')
+  
+# returns JSON object as 
+# a dictionary
+data = json.load(f)
+  
+# Iterating through the json
+# list
+for i in data:
+    for value in i.items():
+        with open(value[1]+".json",'w') as f:
+            f.write("http://luthers-list.herokuapp.com/api/dept/"+value[1]+"/?format=json")
+       
+
