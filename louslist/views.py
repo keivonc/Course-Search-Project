0d533c8a91557e7d10f92891c7a481838e70385a
mwc2dh <mwc2dh@virginia.edu>
2022-10-20 15:37:42 -0400

    removed duplicates

--- louslist/views.py
+++ louslist/views.py
@@ -22,7 +22,7 @@ def find_all_by_dept(request, dept):
 def find_all_by_dept_v2(request, dept):
     filename = "JSON/" + dept + ".json"
     load_json_file(filename)
-    sections = Section.objects.filter(subject=dept)
+    sections = Section.objects.filter(subject=dept).distinct('description')
     return render(request, 'findallbydept.html', {'sections': sections, "department": dept})
 
 def info(request, dept, desc):
