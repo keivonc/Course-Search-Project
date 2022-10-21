bfb6de83c8030a3d3b8a15c9b3b458c2ba0532cc
mwc2dh <mwc2dh@virginia.edu>
2022-10-21 12:31:45 -0400

    fixed syntax errors

--- louslist/views.py
+++ louslist/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 from django.core import serializers
 from .api_loader import *
-from .models import Section
+from .models import Section, Meeting
 
 def load_api(request):
     get_all_json_files()
@@ -22,11 +22,14 @@ def find_all_by_dept(request, dept):
 def find_all_by_dept_v2(request, dept):
     filename = "JSON/" + dept + ".json"
     load_json_file(filename)
-    sections = Section.objects.filter(subject=dept).distinct('description')
+    sections = Section.objects.filter(subject=dept).order_by('description', 'catalog_number').distinct('description')
     return render(request, 'findallbydept.html', {'sections': sections, "department": dept})
 
-def info(request, dept, desc):
+def info(request, dept, desc, ):
     filename = "JSON/" + dept + ".json"
     load_json_file(filename)
     sections = Section.objects.filter(description=desc)
     return render(request, 'des.html', {'sections': sections, "department": dept, "description": desc})
+
+# order by catalog number not description
+# add meeting start time, end time, building location, and course section
