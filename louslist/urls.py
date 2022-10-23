d1f571fbaa52a139348f51d5524d35f960337393
keivonc <keivonc@users.noreply.github.com>
2022-10-23 03:42:16 -0400

    SIS API test cases

--- louslist/urls.py
+++ louslist/urls.py
@@ -23,10 +23,10 @@ from .views import info
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name="index.html")),
+    path('', TemplateView.as_view(template_name="index.html"), name='homepage'),
     path('accounts/', include('allauth.urls')),
     path('logout/', LogoutView.as_view()),
-    path('departments/<str:dept>', find_all_by_dept_v2),
-    path('departments/<str:dept>/<str:cn>/<str:desc>/info', info )
+    path('departments/<str:dept>', find_all_by_dept_v2, name='dept_page'),
+    path('departments/<str:dept>/<str:cn>/<str:desc>/info', info, name='course_page')
     
 ]
