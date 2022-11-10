b91b461ec129eef970ed22b3a1e2609e796eaec3
emilivanov21 <112448206+emilivanov21@users.noreply.github.com>
2022-11-09 22:55:39 -0500

    changes

--- louslist/models.py
+++ louslist/models.py
@@ -69,7 +69,10 @@ class Profile(models.Model):
     bio = models.TextField()
     saved_courses = models.JSONField(default=list, blank=True, null=True)
     saved_sections = models.ManyToManyField(Section, blank=True)
+    
 
-
+class Friend(models.Model):
+    friends = models.ManyToManyField(Profile, blank=True)
+    
     def __str__(self):
         return str(self.user.username) + ", " + str(self.major) + ", " + str(self.year) + ", " + str(self.saved_courses)
