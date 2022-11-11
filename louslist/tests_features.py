99de40d30523c30d162486938bf0475ffba37a80
mwc2dh <mwc2dh@virginia.edu>
2022-11-11 16:30:59 -0500

    update tests

--- louslist/tests_features.py
+++ louslist/tests_features.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from louslist.models import Course, Section, Meeting, User, Profile
 
-class ModelsTests(TestCase):
+class FeaturesTests(TestCase):
     def setUp(self):
         Course.objects.create(subject='WGS', 
                               catalog_number='7500', 
@@ -26,36 +26,12 @@ class ModelsTests(TestCase):
                                end_time='18.00.00.000000-05:00',
                                facility_description='New Cabell Hall 415')
         self.user1 = User.objects.create_user(username='micah')
-
-    def test_course_model(self):
-        wgs7500 = Course.objects.get(subject='WGS')
-        self.assertEqual(wgs7500.subject, 'WGS')
-        self.assertEqual(wgs7500.catalog_number, '7500')
     
-    def test_section_model(self):
-        wgs7500_001 = Section.objects.get(instructor_name='Tiffany King')
-        self.assertEqual(wgs7500_001.course_section, '001')
-
-    def test_meeting_model(self):
-        wgs7500 = Meeting.objects.get(days='Mo')
-        self.assertEqual(wgs7500.start_time, '15.30.00.000000-05:00')
-        self.assertEqual(wgs7500.end_time, '18.00.00.000000-05:00')
-        self.assertEqual(wgs7500.facility_description, 'New Cabell Hall 415')
-    
-    def test_user_model(self):
-        self.assertEqual(self.user1.username, 'micah')
-    
-    def test_profile_model(self):
-        profile = Profile.objects.get(user=self.user1)
-        profile.year = '3rd'
-        profile.major = 'CS'
-        self.assertEqual(profile.user.username, 'micah')
-        self.assertEqual(profile.year, '3rd')
-        self.assertEqual(profile.major, 'CS')
-
-    def tearDown(self):
-        self.user1.delete()
-
-
-    
-
+    # tests:
+    # search for a course
+    # search for an instructor
+    # updating profile
+    # updating password
+    # search for a user
+    # saving a course
+    # unsaving a course
--- louslist/tests_views.py
+++
@@ -1,5 +0,0 @@
-from django.test import TestCase
-
-class viewsTests(TestCase):
-    def temp(self):
-        self.assertEqual('a', 'a')
